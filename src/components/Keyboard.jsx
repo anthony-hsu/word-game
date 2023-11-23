855df08a897a92bc89f5989f8edc86349fdf67aa
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-11-22 16:18:40 -0800

    Add keyboard styling

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -3,7 +3,14 @@ import PropTypes from "prop-types";
 function Keyboard(props) {
   const { letters } = props;
   const showLetters = (key) => {
-    return <div key={`key-${key}`} id={`key-${key}`} className={`keyboard-key keyboard-pos-${letters[key][0]} ${letters[key][1]}`}>{key}</div>;
+    return (<div key={`key-${key}`} id={`key-${key}`} className={`keyboard-key-container keyboard-pos-${letters[key][0]}`}>
+      <div className={`keyboard-key ${letters[key][1]}`}>
+        <p>
+
+        {key}
+        </p>
+        </div>
+      </div>)
   };
   return (
     <div className="keyboard-container">
